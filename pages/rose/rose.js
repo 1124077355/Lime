91888b464187066b011e917ed58df2d8c4dff086
lipeiyu <1754977801>
2019-10-22 16:09:51 +0800

    照明统计
    表格

--- pages/rose/rose.js
+++ pages/rose/rose.js
@@ -1,23 +1,267 @@
-
-var data = [
-  { name: "基站", value: 50, color: "#80e0ed" },
-  { name: "中继", value: 40, color: "#9197ed" },
-  { name: "终端", value: 0, color: "#e4ff99" },
-];
-
+const wxCharts = require("../../utils/wxcharts.js");
+var windowW = 0;
 Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
-    var options = {
-      data: data,
-      legend: "{c}",
-      chartRatio: 0.95,
-      style: "rose",
-      showLegend: true,
-      showLabel: true,
-      animation: true
-    };
-    this.roseComp = this.selectComponent("#rose");
-    this.roseComp.setOptions(options);
-    this.roseComp.initChart(320, 213);
-  }
-});
+
+    // 屏幕宽度
+    this.setData({
+      imageWidth: wx.getSystemInfoSync().windowWidth
+    });
+    console.log(this.data.imageWidth);
+
+    //计算屏幕宽度比列
+    windowW = this.data.imageWidth / 375;
+    console.log(windowW);
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+    //base_rate
+    new wxCharts({
+      canvasId: "base_rate",
+      type: "ring",
+      series: [
+        {
+          name: "已完成",
+          data: 60,
+          //color: '#ff6600'
+        },
+        {
+          name: "未完成",
+          data: 100 - 60,
+          color: '#eeeeee'
+        }
+      ],
+      width: 100,
+      height: 100,
+      dataLabel: false,
+      legend: false,
+      title: { // 显示百分比
+        name: 60 + '%',
+        color: '#333333',
+        fontSize: 14
+      },
+      extra: {
+        pie: {
+          offsetAngle: -90
+        },
+        ringWidth: 6,
+      }
+    });
+
+    //mote_rate
+    new wxCharts({
+      canvasId: "mote_rate",
+      type: "ring",
+      series: [
+        {
+          name: "已完成",
+          data: 80,
+          //color: '#ff6600'
+        },
+        {
+          name: "未完成",
+          data: 100 - 80,
+          color: '#eeeeee'
+        }
+      ],
+      width: 100,
+      height: 100,
+      dataLabel: false,
+      legend: false,
+      title: { // 显示百分比
+        name: 80 + '%',
+        color: '#333333',
+        fontSize: 14
+      },
+      extra: {
+        pie: {
+          offsetAngle: -90
+        },
+        ringWidth: 6,
+      }
+    });
+
+    //terminal_rate
+    new wxCharts({
+      canvasId: "terminal_rate",
+      type: "ring",
+      series: [
+        {
+          name: "已完成",
+          data: 70,
+          //color: '#ff6600'
+        },
+        {
+          name: "未完成",
+          data: 100 - 70,
+          color: '#eeeeee'
+        }
+      ],
+      width: 100,
+      height: 100,
+      dataLabel: false,
+      legend: false,
+      title: { // 显示百分比
+        name: 70 + '%',
+        color: '#333333',
+        fontSize: 14
+      },
+      extra: {
+        pie: {
+          offsetAngle: -90
+        },
+        ringWidth: 6,
+      }
+    });
+
+    //failure rate
+    new wxCharts({
+      animation: true,
+      canvasId: 'failure_rate',
+      type: 'ring',
+      extra: {
+        pie: {
+          offsetAngle: -90
+        },
+        ringWidth: 10,
+      },
+      title: {
+        name: '70%',
+        color: '#7cb5ec',
+        fontSize: 25
+      },
+      subtitle: {
+        name: '故障率',
+        color: '#666666',
+        fontSize: 15
+      },
+      series: [
+        {
+          name: "已完成",
+          data: 70,
+          //color: '#ff6600'
+        },
+        {
+          name: "未完成",
+          data: 100 - 70,
+          color: '#eeeeee'
+        }
+      ],
+      width: (200 * windowW),
+      height: (200 * windowW),
+      dataLabel: false,
+      legend: false,
+      padding: 0
+    });
+
+    //Lighting rate
+    new wxCharts({
+      animation: true,
+      canvasId: 'Lighting_rate',
+      type: 'ring',
+      extra: {
+        pie: {
+          offsetAngle: -90
+        },
+        ringWidth: 10,
+      },
+      title: {
+        name: '70%',
+        color: '#7cb5ec',
+        fontSize: 25
+      },
+      subtitle: {
+        name: '亮灯率',
+        color: '#666666',
+        fontSize: 15
+      },
+      series: [
+        {
+          name: "已完成",
+          data: 70,
+          //color: '#ff6600'
+        },
+        {
+          name: "未完成",
+          data: 100 - 70,
+          color: '#eeeeee'
+        }
+      ],
+      width: (200 * windowW),
+      height: (200 * windowW),
+      dataLabel: false,
+      legend: false,
+      padding: 0
+    });
+
+    //test
+    //test
+    new wxCharts({
+      animation: true,
+      canvasId: 'Electricity_statistics',
+      type: 'line',
+      categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11','12'],
+      yAxis: {
+        title: '用电量 (wh)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      series: [{ // 数据列表
+        name: ' ',
+        data: [0.2, 0.3, 0.4, 0.3, 0.5, 0.4,0.3,0.3,0.4,0.5,0.5,0.4]
+      }],
+      width: 320,
+      height: 200,
+      dataLabel: true, // 是否在图表中显示数据内容值
+      legend: false, // 是否显示图表下方各类别的标识
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve' // (仅对line, area图表有效) 可选值：curve曲线，straight直线 (默认)
+      }
+    });
+
+    //
+    new wxCharts({
+      animation: true,
+      canvasId: 'Fault_statistics',
+      type: 'line',
+      categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+      yAxis: {
+        title: '次数',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      series: [{ // 数据列表
+        name: ' ',
+        data: [1, 0, 1, 2, 2, 0, 0, 0, 0, 1, 1, 0]
+      }],
+      width: 320,
+      height: 200,
+      dataLabel: true, // 是否在图表中显示数据内容值
+      legend: false, // 是否显示图表下方各类别的标识
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve' // (仅对line, area图表有效) 可选值：curve曲线，straight直线 (默认)
+      }
+    });
+  },
+})
